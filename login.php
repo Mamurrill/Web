3f97d459167982e5bfc6bd613c26be1705434f00
viola2020 <60862445+viola2020@users.noreply.github.com>
2020-03-10 20:47:54 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,81 +1,45 @@
 <?php
-// Connects to your Database
-mysql_connect("https\\www.....", "username", "password") or die(mysql_error());
-mysql_select_db("db") or die(mysql_error());
-//Checks if there is a login cookie
-if(isset($_COOKIE['ID_my_site']))
-//if there is, it logs you in and directes you to the members page
-{
-$username = $_COOKIE['ID_my_site'];
-$pass = $_COOKIE['Key_my_site'];
-$check = mysql_query("SELECT * FROM users WHERE username = '$username'")or die(mysql_error());
-while($info = mysql_fetch_array( $check ))
-{
-if ($pass != $info['password'])
-{
-}
-else
-{
-header("Location: userpage.php");
-}
-}
-}
-//if the login form is submitted
-if (isset($_POST['submit'])) { // if form has been submitted
-// makes sure they filled it in
-if(!$_POST['username'] | !$_POST['pass']) {
-die('You did not fill in a required field.');
-}
-// checks it against the database
-if (!get_magic_quotes_gpc()) {
-$_POST['email'] = addslashes($_POST['email']);
-}
-$check = mysql_query("SELECT * FROM users WHERE username = '".$_POST['username']."'")or die(mysql_error());
-//Gives error if user dosen't exist
-$check2 = mysql_num_rows($check);
-if ($check2 == 0) {
-die('That user does not exist in our database. <a href=add.php>Click Here to Register</a>');
-}
-while($info = mysql_fetch_array( $check ))
-{
-$_POST['pass'] = stripslashes($_POST['pass']);
-$info['password'] = stripslashes($info['password']);
-$_POST['pass'] = md5($_POST['pass']);
-//gives error if the password is wrong
-if ($_POST['pass'] != $info['password']) {
-die('Incorrect password, please try again.');
-}
-else
-{
-// if login is ok then we add a cookie
-$_POST['username'] = stripslashes($_POST['username']);
-$hour = time() + 3600;
-setcookie(ID_my_site, $_POST['username'], $hour);
-setcookie(Key_my_site, $_POST['pass'], $hour);
-//then redirect them to the members area
-header("Location: userpage.php");
-}
-}
-}
-else
-{
-// if they are not logged in
-?>
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-<table border="0">
-<tr><td colspan=2><h1>Login</h1></td></tr>
-<tr><td>Username:</td><td>
-<input type="text" name="username" maxlength="40">
-</td></tr>
-<tr><td>Password:</td><td>
-<input type="password" name="pass" maxlength="50">
-</td></tr>
-<tr><td colspan="2" align="right">
-<input type="submit" name="submit" value="Login">
-</td></tr>
-</table>
-</form>
-<?php
-}
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+if (!empty($username) || !empty($password)) {
+$host = "localhost81";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbname = "Bankruptcy";
+
+//create connection
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+if (mysqli_connect_error()) {
+die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+} else {
+$SELECT = "SELECT logID From Login Where logID = ? Limit 1";
+$INSERT = "INSERT Into Login (logID, password, attemptNo, logDate, lName) values(?, ?, ?, ?, ?)";
+
+//Prepare statement
+$stmt = $conn->prepare($SELECT);
+$stmt->bind_param("s", $logID);
+$stmt->execute();
+$stmt->bind_result($logID);
+$stmt->store_result();
+$rnum = #stmt->num_rows;
+
+if ($rnum==0) {
+$stmt->close();
+
+$stmt = $conn->prepare($INSERT);
+$stmt->bind_param("isids", $logID, $password, attemptNo, logDate, lName);
+$stmt->execute();
+echo "New record inserted successfully";
+} else {
+echo "Someone already registered using this username";
+}
+$stmt->close();
+$conn->close();
+}
+} else {
+ echo "No file attached";
+die();
+}
 ?>

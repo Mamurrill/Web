8ac9817c726c5a880cd6bddca1c1d1a18836d450
viola2020 <60862445+viola2020@users.noreply.github.com>
2020-03-18 09:59:28 -0400

    Update process.php

--- process.php
+++ process.php
@@ -1,1 +1,14 @@
-<?php print_r($_FILES);
+<?php
+
+$from="";
+$fname=$_POST['firstname'];
+$lname=$_POST['lastname'];
+$email="fairfield1109@yahoo.com";
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+mail ($fname, $lname, $email, $subject, $message, "FROM:".$from);
+
+Print "Your message has been sent";
+
+?>
